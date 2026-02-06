5d522bfd671e994aec1571314ae0261b617225a6
Punith Sagar P R <138209845+puniTh-sagar44@users.noreply.github.com>
2026-02-06 23:18:42 +0530

    heart.js

--- heart.js
+++ heart.js
@@ -2,11 +2,9 @@ setInterval(() => {
   const heart = document.createElement("div");
   heart.className = "heart";
   heart.innerText = "❤️";
-
   heart.style.left = Math.random() * 100 + "vw";
-  heart.style.fontSize = Math.random() * 18 + 16 + "px";
-
+  heart.style.fontSize = Math.random() * 18 + 14 + "px";
   document.body.appendChild(heart);
 
   setTimeout(() => heart.remove(), 6000);
-}, 400);
+}, 450);
